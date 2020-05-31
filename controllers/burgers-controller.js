bdef7cbb4d58928e28f3c8037bf1ab76c66833a6
olegaposh <oleg.poshiv@gmail.com>
2020-05-30 20:13:56 -0700

    working on html/bootstrap

--- controllers/burgers-controller.js
+++ controllers/burgers-controller.js
@@ -4,24 +4,26 @@ const burger = require("../models/burger.js");
 
 //Create the router for the app, and export the router at the end of your file.
 // Create all our routes and set up logic within those routes where required.
+
+//SELECT ALL
 router.get("/", async (req, res) => {
     const data = await burger.all();
   
     res.render("index", { burgers: data });
   });
-  
-  router.post("/api/cats", async (req, res) => {
-    const data = await cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy]);
+  // CREATE
+  router.post("/api/burgers", async (req, res) => {
+    const data = await burger.create(["burger_name", "devoured"], [req.body.name, req.body.devoured]);
   
     res.json({ id: data.insertId });
   });
-  
-  router.put("/api/cats/:id", async (req, res) => {
+  //UPDATE
+  router.put("/api/burgers/:id", async (req, res) => {
     let condition = `id = ${req.params.id}`;
   
     console.log("condition", condition);
   
-    const data = await cat.update({ sleepy: req.body.sleepy }, condition);
+    const data = await burger.update({ devoured: req.body.devoured }, condition);
   
     if (data.changedRows === 0) {
       res.status(404).end();
@@ -30,17 +32,6 @@ router.get("/", async (req, res) => {
     res.status(200).end();
   });
   
-  router.delete("/api/cats/:id", async (req, res) => {
-    let condition = `id = ${req.params.id}`;
-  
-    const data = await cat.delete(condition);
-  
-    if (data.affectedRows === 0) {
-      res.status(404).end();
-    }
-  
-    res.status(200).end();
-  });
   
   // Export routes for server.js to use.
   module.exports = router;
